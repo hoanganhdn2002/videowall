78a60de06e8165a2dcff5b50c885bce78be0e75a
hoanganhdn2002 <hoanganhdn2002@gmail.com>
2017-04-07 09:21:12 +0700

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -8,7 +8,7 @@ count=0
 status() {
 
         channel_num=$(cat -n /home/$WHOAMI/list_camera.txt | tail -n 1 | cut -f1 | xargs)
-        channel_play=$(ps xua | grep -v grep | grep -v "0:00" | grep ffplay | wc -l)
+        channel_play=$(ps xua | grep ffplay | grep "0:00" | grep -v grep | grep "window_title" | awk '{print $17}' | awk 'END {print NR}')
         name_channel_num=$(cat /home/$WHOAMI/list_camera.txt | awk '{for(i=4;i<=NF;i++) {printf $i ""} ; printf ", "}' | rev | sed s'/,/"/' | rev | sed -r -e 's/^.{0}/&"/' | iconv -f utf8 -t ascii//TRANSLIT)
         channel_die=$(echo -e "$(ps xua | grep ffplay | grep "0:00" | grep -v grep | grep "window_title" | awk '{print $17}' ORS=', ' | rev | sed s'/,/"/' | rev | sed -r -e 's/^.{0}/&"/' )")
         echo -e "{\n error: $channel_play,\n total: $channel_num,\n error_channels: $channel_die\n total_channels: $name_channel_num\n}" | sed 's/error:/"error":/g' | sed 's/total:/"total":/g' | sed 's/error_channels/"error_channels"/g' | sed 's/total_channels/"total_channels"/g' > /tmp/videowall.json
