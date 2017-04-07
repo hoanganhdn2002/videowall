0bf9e093ba3b75faa4eb6abb0e7612b13da5baec
hoanganhdn2002 <hoanganhdn2002@gmail.com>
2017-04-07 08:45:53 +0700

    Update check_channel.sh

--- check_channel.sh
+++ check_channel.sh
@@ -14,22 +14,20 @@ status() {
         echo -e "{\n error: $channel_play,\n total: $channel_num,\n error_channels: $channel_die\n total_channels: $name_channel_num\n}" | sed 's/error:/"error":/g' | sed 's/total:/"total":/g' | sed 's/error_channels/"error_channels"/g' | sed 's/total_channels/"total_channels"/g' > /tmp/videowall.json
         echo -e "{\n error: $channel_play,\n total: $channel_num,\n error_channels: $channel_die\n total_channels: $name_channel_num\n}" | sed 's/error:/"error":/g' | sed 's/total:/"total":/g' | sed 's/error_channels/"error_channels"/g' | sed 's/total_channels/"total_channels"/g'
 }
+  if [ ! -f ${CHECK_TIME} ]; then
+    echo -n "1" > /tmp/time.txt # mặc định 1s ghi ra file json 1 lần
+  fi
 set_time=$(cat /tmp/time.txt) # Tùy chỉnh thời gian sau mỗi lần ghi ra file json
 while true
 do
-  if [ ! -f ${CHECK_TIME} ]; then
-    echo -n "1" > /tmp/time.txt # mặc định 1s ghi ra file json 1 lần
-    else
-
-       if [ $count -eq 0 ]; then
-                count=$(($count+1))
-            elif [ $count -eq 5 ]; then
-                    count=1
+    if [ $count -eq 0 ]; then
+      count=$(($count+1))
+      elif [ $count -eq 5 ]; then
+        count=1
             else
-                    count=$(($count+1))
-            fi
-            status
-            echo "$count "
-            sleep $set_time
-      fi
+              count=$(($count+1))
+    fi
+      status
+      echo "$count "
+      sleep $set_time
 done
